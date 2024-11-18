1dcb81d1d1967a033bb252b3dbcef8cfb7593a77
Franck <52790721+franck-armand@users.noreply.github.com>
2024-11-18 21:24:23 +0100

    - imported get_lung_cancer_ct

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,5 +1,6 @@
 from .mnist import get_mnist
 from .usps import get_usps
 from .lung_cancer import get_lung_cancer
+from .lung_cancer_ct import get_lung_cancer_ct
 
-__all__ = (get_usps, get_mnist, get_lung_cancer)
+__all__ = (get_usps, get_mnist, get_lung_cancer, get_lung_cancer_ct)
